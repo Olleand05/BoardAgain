0483152e667283f94d768107f00b40765cb8e399
HugoLennartsson <hugo.lennartsson@outlook.com>
2026-03-03 16:20:35 +0100

    fixed bonus ability

--- Assets/Scripts/ReturnToMenu.cs
+++ Assets/Scripts/ReturnToMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMenu : MonoBehaviour
 {
+    [Header("Run Reset Settings")]
+    public CharacterData playerAsset;
     public void ExitToTitleScreen()
     {
         SceneManager.LoadScene("TitleScreen");
