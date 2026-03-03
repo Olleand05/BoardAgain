aeea74882deab86818435ac71915cdd8907c96c6
HugoLennartsson <hugo.lennartsson@outlook.com>
2026-03-03 18:17:27 +0100

    Random

--- Assets/Scripts/ReturnAndReset.cs
+++ Assets/Scripts/ReturnAndReset.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class ReturnAndReset : MonoBehaviour
 {
-    [Header("Run Reset Settings")]
-    public CharacterData playerAsset;
-
-
     public void ResetRunAndReturn()
     {
+        CharacterData playerAsset = Resources.Load<CharacterData>("PlayerStats");
+
         if (playerAsset != null)
         {
             playerAsset.bonusAbility = null;
-            Debug.Log("Run data cleared: Bonus Ability reset to null.");
+
+#if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(playerAsset);
+#endif
+
+            Debug.Log("Run data cleared: Bonus Ability reset via Resources.Load.");
         }
         else
         {
-            Debug.LogWarning("ReturnToMenu: No playerAsset assigned! Data was not cleared.");
+            Debug.LogError("ReturnAndReset: Could not find 'PlayerStats' in Resources folder!");
         }
 
         MapManager.currentNodeIndex = 0;
