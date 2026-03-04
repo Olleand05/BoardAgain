e8bb49061c6f5c322d1fc547c779d6848d688cae
Olleand05 <olleand05@gmail.com>
2026-03-04 21:23:12 +0100

    removed unnecessary code

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,4 +1,4 @@
-using System.Collections; // Required for Coroutines
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,20 +8,18 @@ public class MapManager : MonoBehaviour
     public List<NodeType> path = new List<NodeType> { NodeType.Enemy,NodeType.Rest, NodeType.Enemy,NodeType.Rest,NodeType.Boss};
     public GameObject nodePrefab;
     public Transform contentParent;
-    public ScrollRect scrollRect; // Assign this in the Inspector
+    public ScrollRect scrollRect; 
 
     public static int currentNodeIndex = 0;
 
     void Start()
     {
         GenerateMap();
-        // Start the centering process
         StartCoroutine(CenterOnCurrentNode());
     }
 
     void GenerateMap()
     {
-        // Clear existing nodes if any
         foreach (Transform child in contentParent) { Destroy(child.gameObject); }
 
         for (int i = 0; i < path.Count; i++)
@@ -35,16 +33,12 @@ public class MapManager : MonoBehaviour
 
     IEnumerator CenterOnCurrentNode()
     {
-        // Wait for the end of the frame so UI Layout groups can calculate sizes
         yield return new WaitForEndOfFrame();
 
         if (path.Count > 1)
         {
-            // Calculate progress (0 is bottom, 1 is top)
-            // We divide the current index by the total possible steps
             float targetValue = (float)currentNodeIndex / (path.Count - 1);
 
-            // Set the scroll position
             scrollRect.verticalNormalizedPosition = targetValue;
         }
     }
