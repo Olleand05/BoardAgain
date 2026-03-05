f4ef28fe0a77368b74dcab5d7edcbce748667d4c
Olleand05 <olleand05@gmail.com>
2026-03-05 01:34:47 +0100

    testing to revert

---
+++ Assets/Scripts/Abilities/BlockAbility.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using BoardAgain.Characters;
+
+namespace BoardAgain.Abilities
+{
+    [CreateAssetMenu(menuName = "Abilities/BlockAbility")]
+    public class BlockAbility : Ability
+    {
+        [Header("Block Settings")]
+        public int baseShield = 10;
+
+        public override void ActivateAbility(Character caster, Character target)
+        {
+
+            int amount = Mathf.RoundToInt(baseShield + caster.defense);
+            caster.GainShield(amount);
+
+            Debug.Log($"{caster.name} used {name} and gained {amount} shield! (Total shield: {caster.shield})");
+        }
+
+        public string GetRuntimeDescription(Character caster)
+        {
+            int amount = Mathf.RoundToInt(baseShield + caster.defense);
+            return $"{abilityDescription}\nGain {amount} shield.";
+        }
+    }
+}
