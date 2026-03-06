3e255c509125f12bc05938a6228f91b40bdf8d68
Olleand05 <olleand05@gmail.com>
2026-03-06 16:32:18 +0100

    Boss now Has armor

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -22,6 +22,8 @@ namespace BoardAgain.Characters
         public int defense;
         [HideInInspector]
         public int shield;
+        [SerializeField]
+        private GameObject plateArmor;
 
         public Animator animator;
 
@@ -51,6 +53,11 @@ namespace BoardAgain.Characters
                 characterName = "BOSS "+characterName;
                 maxHealth += 50;
                 attack += 10;
+
+                if(plateArmor != null)
+                {
+                    plateArmor.SetActive(true);
+                }
 
                 Debug.Log("Boss Spawned! Health and Attack increased.");
             }
